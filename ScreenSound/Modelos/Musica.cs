41d4ddc3d129af23cc7dd75e30fad7a0602d52e8
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-06-30 07:34:22 -0300

    Deserializando os dados

---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace ScreenSound.Modelos
+{
+    internal class Musica
+    {
+        //esta propriedadde e chamado de [JsonPropertyName... atributo análogo à anotação em outras linguagens
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+        //o ponto de interrogação indica que o compo pode ser nulo
+
+
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+
+        [JsonPropertyName("duration_ms")]
+        public int? Duracao { get; set; }
+
+        [JsonPropertyName("genre")]
+        public string? Genero { get; set; }
+
+        public void ExibirFichaTecnica() {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Música: {Nome} ");
+            Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
+            Console.WriteLine($"Gênero musical: {Genero}");
+        }
+
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,13 +1,29 @@
-﻿
+﻿using ScreenSound.Modelos;
+using System.Text.Json;
+
+//Criação do objeto: HttpClient client = new HttpClient()
 using (HttpClient client = new HttpClient())
+//Escopo do using: As chaves {} delimitam o escopo do using.
+//O objeto client só estará disponível dentro desse bloco de código.
 {
+    //cria uma nova instância da classe HttpClient
+    //Essa instância é atribuída à variável client
     try
     {
+        //A url abaixo e chamado de endpoint
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        //Console.WriteLine(resposta);
+
+        //Desserialização é o processo de converter dados serializados, como JSON, em objetos ou estruturas
+        //de dados utilizáveis em uma linguagem de programação específica.        
+        var musica = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //O ponto de exclamação indica que musica não poder ser nulo
+        //Console.WriteLine(musica.Count);  
+        musica[0].ExibirFichaTecnica();
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
     }
+
 }
